f0a2f232401fd3a5b00b6be8b51910302a85c458
Seonjun Kim <kseonjun@yonsei.ac.kr>
2022-08-05 03:28:57 +0900

    Update progress.py

--- progress.py
+++ progress.py
@@ -2,15 +2,11 @@ import time
 
 
 def progress(idx, max_idx, start_time, extra=None):
-    if idx % (max_idx//10000) == 0:
-        print(f'\r{idx}/{max_idx}', end='')
-        print(f' | TOTAL:{(time.time() - start_time) / (idx + 1) * max_idx:.2f}s', end='')
-        print(f' | TIME:{time.time() - start_time:.2f}s', end='')
-        print(f' | LEFT:{(time.time() - start_time) / (idx + 1) * (max_idx - idx - 1):.2f}s', end='')
+    print(f'\r{idx}/{max_idx} | TOTAL:{(time.time() - start_time) / (idx + 1) * max_idx:.2f}s | TIME:{time.time() - start_time:.2f}s | LEFT:{(time.time() - start_time) / (idx + 1) * (max_idx - idx - 1):.2f}s', end='')
+    pass
+    if extra is not None:
+        print(f' | {extra}', end='')
         pass
-        if extra is not None:
-            print(f' | {extra}', end='')
-            pass
 
 
 def progressEnd(max_idx, start_time):
