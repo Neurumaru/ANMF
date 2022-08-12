148cd4ca237dc7700f8d72dd52e4062c0f1fd447
Neurumaru <kseonjun@yonsei.ac.kr>
2022-08-13 06:55:42 +0900

    Update progress.py

--- progress.py
+++ progress.py
@@ -2,4 +2,7 @@ import time
 
 
 def progress(idx, max_idx, start_time):
-    print(f'\r{idx}/{max_idx} | TOTAL:{(time.time() - start_time) / (idx + 1) * max_idx:.2f}s | TIME:{time.time() - start_time:.2f}s | LEFT:{(time.time() - start_time) / (idx + 1) * (max_idx - idx - 1):.2f}s     ', end='')
+    if idx == 0:
+        print(f'\r{idx}/{max_idx} | TOTAL:{0:.2f}s | TIME:{0:.2f}s | LEFT:{0:.2f}s', end='')
+    else:
+        print(f'\r{idx}/{max_idx} | TOTAL:{(time.time() - start_time) / idx * max_idx:.2f}s | TIME:{time.time() - start_time:.2f}s | LEFT:{(time.time() - start_time) / idx * (max_idx - idx):.2f}s', end='')
