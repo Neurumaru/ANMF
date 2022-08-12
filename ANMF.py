36dcf25568c895fcc206821a0935736e979a26e0
Neurumaru <kseonjun@yonsei.ac.kr>
2022-08-13 06:55:37 +0900

    Update ANMF.py

--- ANMF.py
+++ ANMF.py
@@ -13,12 +13,10 @@ from multiprocessing.pool import Pool
 
 from time import time
 from DataLoader import load_matrix, load_rating_file_as_list, load_negative_file
-from Model import get_model, compile_model, fit_model_one_epoch
+from Model import get_model, compile_model, fit_model_one_epoch, load_weights, save_weights
 from Dataset import get_dataset
-from evaluate import predict_model
+from evaluate import predict_model, calculate_AUC
 from progress import progress
-
-
 
 
 def load_train(data_folder, reverse):
@@ -51,12 +49,141 @@ def load_didra(data_folder):
     return result
 
 
+def grid_search(datafolder, drug, disease, epoch, noise, alpha_beta, ld_delta, phi_psi, num_factors, num_negatives, reverse):
+    max_AUC, max_epoch, max_noise, max_alpha_beta, max_ld_delta, max_phi_psi, max_num_factors, max_num_negatives = 0, 0, 0, 0, 0, 0, 0, 0
+
+    try:
+        with open('outputs/results.pickle', 'rb') as f:
+            results = pickle.load(f)
+    except:
+        results = dict()
+
+    print('=' * 105)
+    print(
+        f'{"AUC":9s}'
+        f'{"EPOCH":9s}'
+        f'{"NOISE":9s}'
+        f'{"ALPAH":9s}'
+        f'{"BETA":9s}'
+        f'{"LAMDA":9s}'
+        f'{"DELTA":9s}'
+        f'{"PHI":9s}'
+        f'{"PSI":9s}'
+        f'{"FACTORS":12s}'
+        f'{"NAGATIVES":12s}')
+    for result in results:
+        AUC = results[result]
+        (e, n, ab, ld, pp, nf, nn) = result
+        print(
+            f'{AUC:<9.4f}'
+            f'{e:<9d}'
+            f'{n:<9.2f}'
+            f'{ab:<9.2f}'
+            f'{ab:<9.2f}'
+            f'{ld:<9.1e}'
+            f'{ld:<9.1e}'
+            f'{pp:<9.2f}'
+            f'{pp:<9.2f}'
+            f'{nf:<12d}'
+            f'{nn:<12d}')
+        if AUC > max_AUC:
+            max_AUC = AUC
+            max_epoch = e
+            max_noise = n
+            max_alpha_beta = ab
+            max_ld_delta = ld
+            max_phi_psi = pp
+            max_num_factors = nf
+            max_num_negatives = nn
+    print('=' * 105)
+    print(
+        f'{max_AUC:<9.4f}'
+        f'{max_epoch:<9d}'
+        f'{max_noise:<9.2f}'
+        f'{max_alpha_beta:<9.2f}'
+        f'{max_alpha_beta:<9.2f}'
+        f'{max_ld_delta:<9.1e}'
+        f'{max_ld_delta:<9.1e}'
+        f'{max_phi_psi:<9.2f}'
+        f'{max_phi_psi:<9.2f}'
+        f'{max_num_factors:<12d}'
+        f'{max_num_negatives:<12d}')
+    print('=' * 105)
+    print(
+        f'{"AUC":9s}'
+        f'{"EPOCH":9s}'
+        f'{"NOISE":9s}'
+        f'{"ALPAH":9s}'
+        f'{"BETA":9s}'
+        f'{"LAMDA":9s}'
+        f'{"DELTA":9s}'
+        f'{"PHI":9s}'
+        f'{"PSI":9s}'
+        f'{"FACTORS":12s}'
+        f'{"NAGATIVES":12s}')
+    for e in epoch:
+        for n in noise:
+            for ab in alpha_beta:
+                for ld in ld_delta:
+                    for pp in phi_psi:
+                        for nf in num_factors:
+                            for nn in num_negatives:
+                                if (e, n, ab, ld, pp, nf, nn) in results:
+                                    continue
+
+                                AUC = ANMF(
+                                    f'{datafolder}', drug=drug, disease=disease, epochs=e,
+                                    num_factors=nf, noise=n, num_negatives=nn,
+                                    alpha=ab, beta=ab, ld=ld, delta=ld, phi=pp, psi=pp, 
+                                    verbose=0, return_AUC=True, save_predict=True, reverse=reverse
+                                )
+                                results[(e, n, ab, ld, pp, nf, nn)] = AUC
+                                if AUC > max_AUC:
+                                    max_AUC = AUC
+                                    max_epoch = e
+                                    max_noise = n
+                                    max_alpha_beta = ab
+                                    max_ld_delta = ld
+                                    max_phi_psi = pp
+                                    max_num_factors = nf
+                                
+                                with open('outputs/results.pickle', 'wb') as f:
+                                    pickle.dump(results, f)
+                                                        
+                                print(
+                                    f'\r{AUC:<9.4f}'
+                                    f'{e:<9d}'
+                                    f'{n:<9.2f}'
+                                    f'{ab:<9.2f}'
+                                    f'{ab:<9.2f}'
+                                    f'{ld:<9.1e}'
+                                    f'{ld:<9.1e}'
+                                    f'{pp:<9.2f}'
+                                    f'{pp:<9.2f}'
+                                    f'{nf:<12d}'
+                                    f'{nn:<12d}')
+    print('=' * 105)
+    print(
+        f'{max_AUC:<9.4f}'
+        f'{max_epoch:<9d}'
+        f'{max_noise:<9.2f}'
+        f'{max_alpha_beta:<9.2f}'
+        f'{max_alpha_beta:<9.2f}'
+        f'{max_ld_delta:<9.1e}'
+        f'{max_ld_delta:<9.1e}'
+        f'{max_phi_psi:<9.2f}'
+        f'{max_phi_psi:<9.2f}'
+        f'{max_num_factors:<12d}'
+        f'{max_num_negatives:<12d}')
+    print('=' * 105)
+
+
 def ANMF(
         data_folder, drug, disease, reverse=False,
         num_factors=256, epochs=50, num_negatives=10,
         noise=0.3, alpha=0.5, beta=0.5, ld=0.5, delta=0.5, phi=0.5, psi=0.5, 
         return_AUC=False, save_predict=True, 
-        learner='adam', learning_rate=0.001, batch_size=1024, verbose=1
+        learner='adam', learning_rate=0.001, batch_size=1024, verbose=1, checkpoint=False
     ):
     if reverse:
         user, item = disease, drug
@@ -88,12 +215,28 @@ def ANMF(
     model, prediction_model = get_model(user, item, num_factors, noise, ld, delta)
     compile_model(model, learner, learning_rate, alpha, beta, phi, psi)
 
-    start_time = time()
-    for epoch in range(epochs):
+    meta = None
+    if checkpoint:
+        model, prediction_model, meta = load_weights(model, prediction_model, meta, f'checkpoints/{data_folder}')
+
+    if meta == None:
+        start_epoch = 0
+        start_time = time()
+        meta = dict()
+    else:
+        start_epoch = meta['epoch']
+        start_time = time() - meta['time']
+
+    for epoch in range(start_epoch, epochs):
         progress(epoch, epochs, start_time)
 
         dataset = get_dataset(train, num_negatives, uSimMat, iSimMat, DiDrAMat, neg_sample, batch_size)
         fit_model_one_epoch(model, dataset, batch_size, verbose=verbose)
+
+        if checkpoint:
+            meta['epoch'] = epoch+1
+            meta['time'] = time() - start_time
+            save_weights(model, prediction_model, meta, f'checkpoints/{data_folder}')
 
         gc.collect()
         tf.keras.backend.clear_session()
@@ -109,119 +252,90 @@ def ANMF(
                     f.write(f'{u}\t{i}\t{pred}\n')
 
     if return_AUC:
-        Pos, Neg = set(), set()
-
-        for user, item, score in test:
-            if score == 1:
-                Pos.add((user, item))
-            else:
-                Neg.add((user, item))
-
-        predict.sort(key=lambda x: -x[2])
-
-        TP, FP = 0, 0
-        TP_sum = 0
-        for user, item, score in predict:
-            if (user, item) in Pos:
-                TP += 1
-            elif (user, item) in Neg:
-                FP += 1
-                TP_sum += TP
-        AUC = TP_sum / (TP * FP)
-
-        return AUC
+        return calculate_AUC(test, predict)
     else:
         return
+    
+
+def usage():
+    print('ANMF')
+    print()
+    print(
+        'Usage: python ANMF.py [type]\n'
+        '         [dataset] [datafolder] [reverse] [epochs] [noise]\n'
+        '         [alpha & beta] [lamda & delta] [phi & psi]\n'
+        '         [num_factors] [num_negatives]')
+    print()
+    print(' - type : evaluete | grid_search')
+    print(' - dataset : atc-code | chemical | original')
+    print(' - datafolder : Disease | Drug')
+    print(' - reverse : 0 | 1')
+    print()
+    print(
+        '  ex) python ANMF.py grid_search\n'
+        '        atc-code Drug0 30,50 0.1\n'
+        '        0.8 1e-04 1\n'
+        '        256,512 10,20')
+    exit()
 
 
-def grid_search():
-    epoch = [30]
-    noise = [0.1, 0.2]
-    alpha_beta = [0.8]
-    ld_delta = [1e-04]
-    phi_psi = [0.5, 1, 2]
-    num_factors = [512]
-    num_negatives = [10]
+def get_argv(index):
+    if len(sys.argv) < index + 1:
+        usage()
+    return sys.argv[index]
 
-    max_AUC, max_epoch, max_noise, max_alpha_beta, max_ld_delta, max_phi_psi, max_num_factors, max_num_negatives = 0, 0, 0, 0, 0, 0, 0, 0
-
-    try:
-        with open('outputs/results.pickle', 'rb') as f:
-            results = pickle.load(f)
-    except:
-        results = dict()
-
-    print(f'=========================================================================================================')
-    print(f'{"AUC":9s}{"EPOCH":9s}{"NOISE":9s}{"ALPAH":9s}{"BETA":9s}{"LAMDA":9s}{"DELTA":9s}{"PHI":9s}{"PSI":9s}{"FACTORS":12s}{"NAGATIVES":12s}')
-    for result in results:
-        AUC = results[result]
-        (e, n, ab, ld, pp, nf, nn) = result
-        print(f'{AUC:<9.4f}{e:<9d}{n:<9.2f}{ab:<9.2f}{ab:<9.2f}{ld:<9.1e}{ld:<9.1e}{pp:<9.2f}{pp:<9.2f}{nf:<12d}{nn:<12d}')
-        if AUC > max_AUC:
-            max_AUC = AUC
-            max_epoch = e
-            max_noise = n
-            max_alpha_beta = ab
-            max_ld_delta = ld
-            max_phi_psi = pp
-            max_num_factors = nf
-            max_num_negatives = nn
-    print(f'=========================================================================================================')
-    print(f'{max_AUC:<9.4f}{max_epoch:<9d}{max_noise:<9.2f}{max_alpha_beta:<9.2f}{max_alpha_beta:<9.2f}{max_ld_delta:<9.1e}{max_ld_delta:<9.1e}{max_phi_psi:<9.2f}{max_phi_psi:<9.2f}{max_num_factors:<12d}{max_num_negatives:<12d}')
-    print(f'=========================================================================================================')
-    print(f'{"AUC":9s}{"EPOCH":9s}{"NOISE":9s}{"ALPAH":9s}{"BETA":9s}{"LAMDA":9s}{"DELTA":9s}{"PHI":9s}{"PSI":9s}{"FACTORS":12s}{"NAGATIVES":12s}')
-    for e in epoch:
-        for n in noise:
-            for ab in alpha_beta:
-                for ld in ld_delta:
-                    for pp in phi_psi:
-                        for nf in num_factors:
-                            for nn in num_negatives:
-                                if (e, n, ab, ld, pp, nf, nn) in results:
-                                    continue
-
-                                AUC = ANMF(
-                                    f'Drug0', drug=3245, disease=6322, epochs=e,
-                                    num_factors=nf, noise=n, num_negatives=nn,
-                                    alpha=ab, beta=ab, ld=ld, delta=ld, phi=pp, psi=pp, 
-                                    return_AUC=True, save_predict=True
-                                )
-                                results[(e, n, ab, ld, pp, nf, nn)] = AUC
-                                if AUC > max_AUC:
-                                    max_AUC = AUC
-                                    max_epoch = e
-                                    max_noise = n
-                                    max_alpha_beta = ab
-                                    max_ld_delta = ld
-                                    max_phi_psi = pp
-                                    max_num_factors = nf
-                                
-                                with open('outputs/results.pickle', 'wb') as f:
-                                    pickle.dump(results, f)
-                                                        
-                                print(f'\r{AUC:<9.4f}{e:<9d}{n:<9.2f}{ab:<9.2f}{ab:<9.2f}{ld:<9.1e}{ld:<9.1e}{pp:<9.2f}{pp:<9.2f}{nf:<12d}{nn:<12d}')
-    print(f'=========================================================================================================')
-    print(f'{max_AUC:<9.4f}{max_epoch:<9d}{max_noise:<9.2f}{max_alpha_beta:<9.2f}{max_alpha_beta:<9.2f}{max_ld_delta:<9.1e}{max_ld_delta:<9.1e}{max_phi_psi:<9.2f}{max_phi_psi:<9.2f}{max_num_factors:<12d}{max_num_negatives:<12d}')
-    print(f'=========================================================================================================')
 
 if __name__ == '__main__':
-    # grid_search()
+    exetype = get_argv(1)
+    dataset = get_argv(2)
+    datafolder = get_argv(3)
+    reverse = get_argv(4)
 
-    file_path = sys.argv[1]
-    
-    if len(sys.argv) != 3:
-        print('ANMF')
-        print()
-        print('usage: python ANMF.py [Disease | Drug] [0-9]')
-        exit()
+    if dataset == 'atc-code':
+        drug = 3245
+        disease = 6322
+    elif dataset == 'chemical':
+        drug = 11219
+        disease = 6322
+    elif dataset == 'original':
+        drug = 593
+        disease = 313
+    else:
+        usage()
 
-    fold = sys.argv[1]
-    i = sys.argv[2]
+    if reverse == '0':
+        reverse = False
+    elif reverse == '1':
+        reverse = True
+    else:
+        usage()
 
-    AUC = ANMF(
-        f'{fold}{i}', drug=3245, disease=6322, epochs=50,
-        num_factors=512, noise=0.1, num_negatives=10,
-        alpha=0.8, beta=0.8, ld=1e-4, delta=1e-4, phi=1, psi=1, 
-        return_AUC=True, save_predict=True, verbose=0, reverse=True
-    )
-    print(f'\{fold}{i}: {AUC}')
+    if exetype == 'evaluate':
+        epoch = int (get_argv(5))
+        noise = float(get_argv(6))
+        alpha_beta = float(get_argv(7))
+        ld_delta = float(get_argv(8))
+        phi_psi = float(get_argv(9))
+        num_factors = int(get_argv(10))
+        num_negatives = int(get_argv(11))
+
+        AUC = ANMF(
+            f'{datafolder}', drug=drug, disease=disease, epochs=epoch,
+            num_factors=num_factors, noise=noise, num_negatives=num_negatives,
+            alpha=alpha_beta, beta=alpha_beta, ld=ld_delta, delta=ld_delta, phi=phi_psi, psi=phi_psi, 
+            return_AUC=True, save_predict=True, verbose=0, reverse=reverse, checkpoint=True
+        )
+        print(f'\r{datafolder}: {AUC}')
+    elif exetype == 'grid_search':
+        epoch = list(map(int, get_argv(5).split(',')))
+        noise = list(map(float, get_argv(6).split(',')))
+        alpha_beta = list(map(float, get_argv(7).split(',')))
+        ld_delta = list(map(float, get_argv(8).split(',')))
+        phi_psi = list(map(float, get_argv(9).split(',')))
+        num_factors = list(map(int, get_argv(10).split(',')))
+        num_negatives = list(map(int, get_argv(11).split(',')))
+
+        grid_search(datafolder, drug, disease, epoch, noise, alpha_beta, ld_delta, phi_psi, num_factors, num_negatives, reverse)
+    else:
+        usage()
+        
